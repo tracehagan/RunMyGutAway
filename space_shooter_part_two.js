47a3d00e3b7a63bacb5a99f1256c36492bdce1f6
TraceHagan <trace_hagan@yahoo.com>
2013-06-28 11:44:03 -0700

    added Brad, new background

--- space_shooter_part_two.js
+++ space_shooter_part_two.js
@@ -112,10 +112,11 @@ var imageRepository = new function() {
 	this.bullet = new Image();
 	this.background2 = new Image();
 	this.background3 = new Image();
+	this.background4 = new Image();
 	this.carrot = new Image();
 
 	// Ensure all images have loaded before starting the game
-	var numImages = 6;
+	var numImages = 7;
 	var numLoaded = 0;
 	function imageLoaded() {
 		numLoaded++;
@@ -141,14 +142,18 @@ var imageRepository = new function() {
 	this.carrot.onload=function() {
 		imageLoaded();
 	}
+	this.background4.onload=function() {
+		imageLoaded();
+	}
 
 	// Set images src
 	this.background.src = "imgs/bg.png";
-	this.spaceship.src = "imgs/BigBoy2.png";
+	this.spaceship.src = "imgs/BRAD.png";
 	this.bullet.src = "imgs/bullet.gif";
 	this.background2.src = "imgs/lavabg.jpg";
 	this.background3.src = "imgs/skybg.jpg";
 	this.carrot.src = "imgs/Carrot.gif";
+	this.background4.src = "imgs/desertbg.jpg";
 }
 
 
@@ -191,7 +196,7 @@ function Background() {
 	this.draw = function() {
 		// Pan background
 		this.x -= this.speed;
-		switch (gamespeed % 3){
+		switch (gamespeed % 4){
 			case 0: 
 				this.context.drawImage(imageRepository.background, this.x, this.y);
 				// Draw another image at the top edge of the first image
@@ -206,6 +211,11 @@ function Background() {
 				this.context.drawImage(imageRepository.background3, this.x, this.y);
 				// Draw another image at the top edge of the first image
 				this.context.drawImage(imageRepository.background3, this.x + this.canvasWidth, this.y);
+				break;
+			case 3:
+				this.context.drawImage(imageRepository.background4, this.x, this.y);
+				// Draw another image at the top edge of the first image
+				this.context.drawImage(imageRepository.background4, this.x + this.canvasWidth, this.y);
 				break;
 			}
 
